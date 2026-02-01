5c7bce8174c82e483a594ec1368f9026a1bd5411
Alex <you@example.com>
2026-02-01 17:23:51 +0000

    Add import for WhisperDataCollator in dataset module

--- asr_sr/datasets/__init__.py
+++ asr_sr/datasets/__init__.py
@@ -1,2 +1,3 @@
 from .readers import TSVFileReader, SegmentsTSVReader, YodasCTCReader, HFDatasetReader
 from .whisper import HFWhisperDataset, YodasWhisperDataset, CommonVoiceWhisperDataset, BookWhisperDataset, WhisperDataset
+from .collators import WhisperDataCollator
