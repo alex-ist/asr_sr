81e8723c6a27551b01dd01e33dbc60030cee68c5
Alex <you@example.com>
2026-02-08 10:46:46 +0000

    added WhisperSubset WhisperConcatDataset

--- asr_sr/datasets/__init__.py
+++ asr_sr/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .readers import TSVFileReader, SegmentsTSVReader, YodasCTCReader, HFDatasetReader
 from .whisper import HFWhisperDataset, YodasWhisperDataset, CommonVoiceWhisperDataset, BookWhisperDataset, WhisperDataset
+from .whisper import WhisperSubset, WhisperConcatDataset
 from .collators import WhisperDataCollator
